a49d71a181fb463b08a28280b9c2928dbb628156
aivanilov <ivanilov@inbox.ru>
2023-05-04 10:17:58 +0700

    Unused inputs removed.

--- src/main/java/org/example/StackingFuckingGood.java
+++ src/main/java/org/example/StackingFuckingGood.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class StackingFuckingGood {
--- src/test/java/org/example/StackingFuckingGoodTest.java
+++ src/test/java/org/example/StackingFuckingGoodTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class StackingFuckingGoodTest {
